cd4853fe753eae44c343c7e7c4673b1d4ed4b87c
Carlos <carlosbolorino@hotmail.com>
2022-06-06 12:53:19 +0200

    add proyecto

--- OpTask_MERN/frontend/src/components/FormularioProyecto.tsx
+++ OpTask_MERN/frontend/src/components/FormularioProyecto.tsx
@@ -1,21 +1,18 @@
-import { useState } from "react";
-
-interface IProyecto {
-  nombre: string;
-  descripcion: string;
-  fechaEntrega: Date;
-  nombreCliente: string;
-}
+import { FormEvent, useState } from "react";
+import { IProyecto } from "../context/ProyectosProvider";
+import useProyectos from "../hooks/useProyectos";
+import Alert from "./Alert";
 
 const FormularioProyecto = () => {
   const initialState = {
     nombre: "",
     descripcion: "",
-    fechaEntrega: new Date(Date.now()),
-    nombreCliente: "",
+    fechaEntrega: "",
+    cliente: "",
   };
 
-  const [proyecto, setProyecto] = useState<IProyecto>(initialState);
+  const [proyecto, setProyecto] = useState(initialState);
+  const { showAlert, alert, submitProyecto } = useProyectos();
 
   const handleChange = <P extends keyof IProyecto>(
     name: P,
@@ -27,8 +24,25 @@ const FormularioProyecto = () => {
     });
   };
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(proyecto);
+    if (Object.values(proyecto).includes("")) {
+      showAlert({ message: "Todos los campos son obligatorios", error: true });
+      return;
+    }
+
+    await submitProyecto(proyecto);
+    setProyecto(initialState);
+  };
+
   return (
-    <form className="bg-white py-10 px-5 md:w-1/2 rounded-lg">
+    <form
+      className="bg-white py-10 px-5 md:w-1/2 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      {alert && <Alert {...alert} />}
       <div>
         <label
           className="text.gray-700 uppercase font-bold text-sm"
@@ -41,6 +55,7 @@ const FormularioProyecto = () => {
           type={"text"}
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           placeholder="Nombre del proyecto"
+          value={proyecto.nombre}
           onChange={(e) => handleChange("nombre", e.target.value)}
         ></input>
       </div>
@@ -71,24 +86,24 @@ const FormularioProyecto = () => {
           type={"date"}
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           placeholder="Nombre del proyecto"
-          onChange={(e) =>
-            handleChange("fechaEntrega", new Date(e.target.value))
-          }
+          value={proyecto.fechaEntrega}
+          onChange={(e) => handleChange("fechaEntrega", e.target.value)}
         ></input>
       </div>
       <div>
         <label
           className="text.gray-700 uppercase font-bold text-sm"
-          htmlFor="nombre-cliente"
+          htmlFor="cliente"
         >
-          Nombre proyecto
+          Cliente
         </label>
         <input
-          id="nombre-cliente"
+          id="cliente"
           type={"text"}
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           placeholder="Nombre del cliente"
-          onChange={(e) => handleChange("nombreCliente", e.target.value)}
+          value={proyecto.cliente}
+          onChange={(e) => handleChange("cliente", e.target.value)}
         ></input>
       </div>
       <input
--- OpTask_MERN/frontend/src/context/ProyectosProvider.tsx
+++ OpTask_MERN/frontend/src/context/ProyectosProvider.tsx
@@ -1,13 +1,28 @@
 import { useState, useEffect, createContext, ReactNode, FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { IAlert } from "../components/Alert";
 import axiosClient from "../config/axiosClient";
 
-interface IProyecto {}
+export interface IProyecto {
+  nombre: string;
+  descripcion: string;
+  fechaEntrega: string;
+  cliente: string;
+}
 
 interface IProyectosContext {
   proyectos: IProyecto[];
+  submitProyecto: (proyecto: IProyecto) => void;
+  showAlert: (alert: IAlert) => void;
+  alert: IAlert | undefined;
 }
 
-const ProyectosContext = createContext<IProyectosContext>({ proyectos: [] });
+const ProyectosContext = createContext<IProyectosContext>({
+  proyectos: [],
+  submitProyecto: (proyecto: IProyecto) => {},
+  showAlert: (alert: IAlert) => {},
+  alert: undefined,
+});
 
 interface IProyectosProviderComponent {
   children: ReactNode;
@@ -15,9 +30,44 @@ interface IProyectosProviderComponent {
 
 const ProyectosProvider: FC<IProyectosProviderComponent> = ({ children }) => {
   const [proyectos, setProyectos] = useState([]);
+  const [alert, setAlert] = useState<IAlert | undefined>();
+  const navigate = useNavigate();
+
+  const showAlert = (alert: IAlert) => {
+    setAlert(alert);
+
+    setTimeout(() => setAlert(undefined), 3000);
+  };
+
+  const submitProyecto = async (proyecto: IProyecto) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) return;
+
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await axiosClient.post("/proyectos", proyecto, config);
+      setAlert({ message: "Proyecto creado", error: false });
+
+      setTimeout(() => {
+        setAlert(undefined);
+        navigate("/proyectos");
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <ProyectosContext.Provider value={{ proyectos }}>
+    <ProyectosContext.Provider
+      value={{ proyectos, submitProyecto, showAlert, alert }}
+    >
       {children}
     </ProyectosContext.Provider>
   );
